40ba86585a53e3149a7216d39cc3ad916d613a15
Joaquin Flores <paul_1705@outlook.com>
2020-10-15 12:58:12 -0500

    Menu consola

---
+++ src/joaquin/portafolio/controllers/Register.java
@@ -0,0 +1,48 @@
+package joaquin.portafolio.controllers;
+
+import joaquin.portafolio.storage.Database;
+import joaquin.portafolio.views.View;
+
+public class Register {
+
+    private Database database;
+
+    public Register(){
+        database = new Database();
+    }
+
+    //register inicia el proceso de la registradora
+    public void register(){
+        View.showHeaderPrincipal();
+        View.showMenuPrincipal();
+        int option;
+        do {
+            View.showGetOption();
+            option = View.getOption();
+            switch(option){
+                case 1:
+                    View.showBuyHeader();
+                    break;
+                case 2:
+                    View.showSaleHeader();
+                    break;
+                case 3:
+                    View.showStockHeader();
+                    break;
+                case 4:
+                    View.showBuyListHeader();
+                    break;
+                case 5:
+                    View.showSaleListHeader();
+                    break;
+                case 6:
+                    View.showExitProgram();
+                    // TODO agregar la salida del sistema
+                    break;
+                default:
+                    View.showInvalidOption();
+            }
+        }while (option < 1 || option >6);
+
+    }
+}
--- src/joaquin/portafolio/test/Main.java
+++ src/joaquin/portafolio/test/Main.java
@@ -1,13 +1,18 @@
 package joaquin.portafolio.test;
 
-import joaquin.portafolio.objects.Product;
-import joaquin.portafolio.storage.Database;
+import joaquin.portafolio.controllers.Register;
+import joaquin.portafolio.views.View;
 
 public class Main {
     public static void main(String[] args){
+        /*
         Database database = new Database();
         Product product = database.getByIndex(0);
         System.out.println(product);
         product.setAmount(10);
+
+         */
+        Register register = new Register();
+        register.register();
     }
 }
